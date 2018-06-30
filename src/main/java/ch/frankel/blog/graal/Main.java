3a861246b78038cf3cbf051831ef6114028dc12f
Nicolas Fränkel <nicolas@frankel.ch>
2018-06-30 21:30:32 +0200

    Configure for Class.getDeclaredField()

--- src/main/java/ch/frankel/blog/graal/Main.java
+++ src/main/java/ch/frankel/blog/graal/Main.java
@@ -1,10 +1,14 @@
 package ch.frankel.blog.graal;
 
+import java.lang.reflect.Field;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
         Reflection reflection = new Reflection();
         Class<?> clazz = reflection.classForName();
-        clazz.newInstance();
+        Object o = clazz.newInstance();
+        Field field = clazz.getDeclaredField("label");
+        field.get(o);
     }
 }
