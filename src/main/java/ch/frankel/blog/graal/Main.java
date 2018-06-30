a83a72cd5f4d15e58deb4f10895628709e3d5df8
Nicolas Fränkel <nicolas@frankel.ch>
2018-06-30 21:12:37 +0200

    Configure for Class.newInstance()

--- src/main/java/ch/frankel/blog/graal/Main.java
+++ src/main/java/ch/frankel/blog/graal/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Reflection reflection = new Reflection();
-        reflection.classForName();
+        Class<?> clazz = reflection.classForName();
+        clazz.newInstance();
     }
 }
